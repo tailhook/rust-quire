b599fb3304ff28844a2d6ee6cec46a1fbf530d9e
Paul Colomiets <paul@colomiets.name>
2014-09-14 15:22:34 +0000

    Implement encoder interface (partially)

--- src/emit.rs
+++ src/emit.rs
@@ -1,9 +1,11 @@
-use std::io::IoResult;
-use std::str::from_utf8;
-#[cfg(test)] use std::io::MemWriter;
+use std::io::{IoResult, IoError};
+use std::io::{MemWriter, IoError};
+use std::io::Writer;
+use std::str::{from_char, from_utf8};
+use std::to_str::ToStr;
+use serialize::{Encodable, Encoder};
 
 use super::parser::Node;
-#[cfg(test)] use super::parser::parse;
 
 use S = self::State;
 use L = self::Line;
@@ -241,6 +243,14 @@ impl<'a> Context<'a> {
         }
         return Ok(());
     }
+
+    fn to_buffer<'x, T: Encodable<Context<'x>, IoError>>(
+        val: &T, wr: &'x mut MemWriter)
+    {
+        let mut encoder = Context::new(wr);
+        val.encode(&mut encoder);
+    }
+
 }
 
 pub fn emit_parse_tree(tree: &Node, stream: &mut Writer)
@@ -250,88 +260,246 @@ pub fn emit_parse_tree(tree: &Node, stream: &mut Writer)
     return ctx.emit_node(tree);
 }
 
-#[cfg(test)]
-fn emit_and_compare(list: &[Opcode], output: &str) {
-    let mut buf = MemWriter::new();
-    {
-        let mut ctx = Context::new(&mut buf);
-        for op in list.iter() {
-            ctx.emit(*op);
-        }
+
+impl<'a> Encoder<IoError> for Context<'a> {
+    fn emit_nil(&mut self) -> Result<(), IoError> {
+        return self.emit(Null(None, None, Null::Nothing));
     }
-    let bytes = buf.unwrap();
-    let value = from_utf8(bytes.as_slice()).unwrap();
-    assert_eq!(value, output);
-}
-
-#[test]
-fn test_empty() {
-    emit_and_compare([
-        Null(None, None, Null::Nothing),
-    ], r#"
-"#);
-}
-
-#[test]
-fn test_plain() {
-    emit_and_compare([
-        Scalar(None, None, Auto, "hello"),
-    ], r#"hello
-"#);
-}
-
-#[test]
-fn test_map() {
-    emit_and_compare([
-        MapStart(None, None),
-        Scalar(None, None, Auto, "a"),
-        Scalar(None, None, Auto, "val"),
-        Scalar(None, None, Auto, "b"),
-        Scalar(None, None, Auto, "2"),
-        MapEnd,
-    ], r#"a: val
-b: 2
-"#);
+    fn emit_uint(&mut self, v: uint) -> Result<(), IoError> {
+        let val = v.to_str();
+        return self.emit(Scalar(None, None, Plain, val.as_slice()));
+    }
+    fn emit_u64(&mut self, v: u64) -> Result<(), IoError> {
+        let val = v.to_str();
+        return self.emit(Scalar(None, None, Plain, val.as_slice()));
+    }
+    fn emit_u32(&mut self, v: u32) -> Result<(), IoError> {
+        let val = v.to_str();
+        return self.emit(Scalar(None, None, Plain, val.as_slice()));
+    }
+    fn emit_u16(&mut self, v: u16) -> Result<(), IoError> {
+        let val = v.to_str();
+        return self.emit(Scalar(None, None, Plain, val.as_slice()));
+    }
+    fn emit_u8(&mut self, v: u8) -> Result<(), IoError> {
+        let val = v.to_str();
+        return self.emit(Scalar(None, None, Plain, val.as_slice()));
+    }
+    fn emit_int(&mut self, v: int) -> Result<(), IoError> {
+        let val = v.to_str();
+        return self.emit(Scalar(None, None, Plain, val.as_slice()));
+    }
+    fn emit_i64(&mut self, v: i64) -> Result<(), IoError> {
+        let val = v.to_str();
+        return self.emit(Scalar(None, None, Plain, val.as_slice()));
+    }
+    fn emit_i32(&mut self, v: i32) -> Result<(), IoError> {
+        let val = v.to_str();
+        return self.emit(Scalar(None, None, Plain, val.as_slice()));
+    }
+    fn emit_i16(&mut self, v: i16) -> Result<(), IoError> {
+        let val = v.to_str();
+        return self.emit(Scalar(None, None, Plain, val.as_slice()));
+    }
+    fn emit_i8(&mut self, v: i8) -> Result<(), IoError> {
+        let val = v.to_str();
+        return self.emit(Scalar(None, None, Plain, val.as_slice()));
+    }
+    fn emit_bool(&mut self, v: bool) -> Result<(), IoError> {
+        return self.emit(Scalar(None, None, Plain,
+            if v { "true" } else { "false" }));
+    }
+    fn emit_f64(&mut self, v: f64) -> Result<(), IoError> {
+        let val = v.to_str();
+        return self.emit(Scalar(None, None, Plain, val.as_slice()));
+    }
+    fn emit_f32(&mut self, v: f32) -> Result<(), IoError> {
+        let val = v.to_str();
+        return self.emit(Scalar(None, None, Plain, val.as_slice()));
+    }
+    fn emit_char(&mut self, v: char) -> Result<(), IoError> {
+        let val = from_char(v);
+        return self.emit(Scalar(None, None, Auto, val.as_slice()));
+    }
+    fn emit_str(&mut self, v: &str) -> Result<(), IoError> {
+        return self.emit(Scalar(None, None, Auto, v));
+    }
+    fn emit_enum(&mut self, name: &str, f: |&mut Context<'a>| -> Result<(), IoError>) -> Result<(), IoError> {
+        unimplemented!();
+    }
+    fn emit_enum_variant(&mut self, v_name: &str, v_id: uint, len: uint, f: |&mut Context<'a>| -> Result<(), IoError>) -> Result<(), IoError> {
+        unimplemented!();
+    }
+    fn emit_enum_variant_arg(&mut self, a_idx: uint, f: |&mut Context<'a>| -> Result<(), IoError>) -> Result<(), IoError> {
+        unimplemented!();
+    }
+    fn emit_enum_struct_variant(&mut self, v_name: &str, v_id: uint, len: uint, f: |&mut Context<'a>| -> Result<(), IoError>) -> Result<(), IoError> {
+        unimplemented!();
+    }
+    fn emit_enum_struct_variant_field(&mut self, f_name: &str, f_idx: uint, f: |&mut Context<'a>| -> Result<(), IoError>) -> Result<(), IoError> {
+        unimplemented!();
+    }
+    fn emit_struct(&mut self, name: &str, len: uint, f: |&mut Context<'a>| -> Result<(), IoError>) -> Result<(), IoError> {
+        self.emit(MapStart(None, None))
+        .and(f(self))
+        .and(self.emit(MapEnd))
+    }
+    fn emit_struct_field(&mut self, f_name: &str, f_idx: uint, f: |&mut Context<'a>| -> Result<(), IoError>) -> Result<(), IoError> {
+        self.emit(Scalar(None, None, Auto, f_name))
+        .and(f(self))
+    }
+    fn emit_tuple(&mut self, len: uint, f: |&mut Context<'a>| -> Result<(), IoError>) -> Result<(), IoError> {
+        unimplemented!();
+    }
+    fn emit_tuple_arg(&mut self, idx: uint, f: |&mut Context<'a>| -> Result<(), IoError>) -> Result<(), IoError> {
+        unimplemented!();
+    }
+    fn emit_tuple_struct(&mut self, name: &str, len: uint, f: |&mut Context<'a>| -> Result<(), IoError>) -> Result<(), IoError> {
+        unimplemented!();
+    }
+    fn emit_tuple_struct_arg(&mut self, f_idx: uint, f: |&mut Context<'a>| -> Result<(), IoError>) -> Result<(), IoError> {
+        unimplemented!();
+    }
+    fn emit_option(&mut self, f: |&mut Context<'a>| -> Result<(), IoError>) -> Result<(), IoError> {
+        unimplemented!();
+    }
+    fn emit_option_none(&mut self) -> Result<(), IoError> {
+        unimplemented!();
+    }
+    fn emit_option_some(&mut self, f: |&mut Context<'a>| -> Result<(), IoError>) -> Result<(), IoError> {
+        unimplemented!();
+    }
+    fn emit_seq(&mut self, len: uint, f: |this: &mut Context<'a>| -> Result<(), IoError>) -> Result<(), IoError> {
+        unimplemented!();
+    }
+    fn emit_seq_elt(&mut self, idx: uint, f: |this: &mut Context<'a>| -> Result<(), IoError>) -> Result<(), IoError> {
+        unimplemented!();
+    }
+    fn emit_map(&mut self, len: uint, f: |&mut Context<'a>| -> Result<(), IoError>) -> Result<(), IoError> {
+        unimplemented!();
+    }
+    fn emit_map_elt_key(&mut self, idx: uint, f: |&mut Context<'a>| -> Result<(), IoError>) -> Result<(), IoError> {
+        unimplemented!();
+    }
+    fn emit_map_elt_val(&mut self, idx: uint, f: |&mut Context<'a>| -> Result<(), IoError>) -> Result<(), IoError> {
+        unimplemented!();
+    }
 }
 
 #[cfg(test)]
-fn assert_yaml_eq_yaml(source: &'static str, output: &'static str) {
-    let mut buf = MemWriter::new();
-    parse(source, |doc| {
-        let mut ctx = Context::new(&mut buf);
-        ctx.emit_node(&doc.root).unwrap();
-    }).unwrap();
-    let bytes = buf.unwrap();
-    let value = from_utf8(bytes.as_slice()).unwrap();
-    assert_eq!(value, output);
-}
+mod test {
+    use std::io::{MemWriter, IoError};
+    use std::str::{from_utf8};
+    use std::mem::transmute;
+    use serialize::{Encodable, Encoder};
 
-#[test]
-fn yaml_scalar() {
-    assert_yaml_eq_yaml("Hello", "Hello\n");
-}
+    use super::super::parser::parse;
+    use super::{Opcode, Context, Null, Auto, Scalar, MapStart, MapEnd};
 
-#[test]
-fn yaml_map() {
-    assert_yaml_eq_yaml("a: b\nc: d", "a: b\nc: d\n");
-}
+    fn emit_and_compare(list: &[Opcode], output: &str) {
+        let mut buf = MemWriter::new();
+        {
+            let mut ctx = Context::new(&mut buf);
+            for op in list.iter() {
+                ctx.emit(*op);
+            }
+        }
+        let bytes = buf.unwrap();
+        let value = from_utf8(bytes.as_slice()).unwrap();
+        assert_eq!(value, output);
+    }
 
-#[test]
-fn yaml_map_map() {
-    assert_yaml_eq_yaml("a:\n b: c", "a:\n  b: c\n");
-}
+    #[test]
+    fn test_empty() {
+        emit_and_compare([
+            Null(None, None, Null::Nothing),
+        ], "\n");
+    }
 
-#[test]
-fn yaml_list() {
-    assert_yaml_eq_yaml("- a\n- b", "- a\n- b\n");
-}
+    #[test]
+    fn test_plain() {
+        emit_and_compare([
+            Scalar(None, None, Auto, "hello"),
+        ], "hello\n");
+    }
 
-#[test]
-fn yaml_map_list() {
-    assert_yaml_eq_yaml("a:\n- b\n- c", "a:\n- b\n- c\n");
-}
+    #[test]
+    fn test_map() {
+        emit_and_compare([
+            MapStart(None, None),
+            Scalar(None, None, Auto, "a"),
+            Scalar(None, None, Auto, "val"),
+            Scalar(None, None, Auto, "b"),
+            Scalar(None, None, Auto, "2"),
+            MapEnd,
+        ], "a: val\nb: 2\n");
+    }
 
-#[test]
-fn yaml_list_map() {
-    assert_yaml_eq_yaml("- a: b\n  c: d", "- a: b\n  c: d\n");
+    fn assert_yaml_eq_yaml(source: &'static str, output: &'static str) {
+        let mut buf = MemWriter::new();
+        parse(source, |doc| {
+            let mut ctx = Context::new(&mut buf);
+            ctx.emit_node(&doc.root).unwrap();
+        }).unwrap();
+        let bytes = buf.unwrap();
+        let value = from_utf8(bytes.as_slice()).unwrap();
+        assert_eq!(value, output);
+    }
+
+    #[test]
+    fn yaml_scalar() {
+        assert_yaml_eq_yaml("Hello", "Hello\n");
+    }
+
+    #[test]
+    fn yaml_map() {
+        assert_yaml_eq_yaml("a: b\nc: d", "a: b\nc: d\n");
+    }
+
+    #[test]
+    fn yaml_map_map() {
+        assert_yaml_eq_yaml("a:\n b: c", "a:\n  b: c\n");
+    }
+
+    #[test]
+    fn yaml_list() {
+        assert_yaml_eq_yaml("- a\n- b", "- a\n- b\n");
+    }
+
+    #[test]
+    fn yaml_map_list() {
+        assert_yaml_eq_yaml("a:\n- b\n- c", "a:\n- b\n- c\n");
+    }
+
+    #[test]
+    fn yaml_list_map() {
+        assert_yaml_eq_yaml("- a: b\n  c: d", "- a: b\n  c: d\n");
+    }
+
+    #[test]
+    fn encode_int() {
+        let mut buf = MemWriter::new();
+        Context::to_buffer(&1u, &mut buf);
+        let bytes = buf.unwrap();
+        let value = from_utf8(bytes.as_slice()).unwrap();
+        assert_eq!(value, "1\n");
+    }
+
+    #[deriving(Encodable)]
+    struct Something {
+        key1: int,
+        key2: String,
+    }
+
+    #[test]
+    fn encode_struct() {
+        let mut buf = MemWriter::new();
+        Context::to_buffer(&Something{
+            key1: -123,
+            key2: "hello".to_string(),
+            }, &mut buf);
+        let bytes = buf.unwrap();
+        let value = from_utf8(bytes.as_slice()).unwrap();
+        assert_eq!(value, "key1: -123\nkey2: hello\n");
+    }
 }
