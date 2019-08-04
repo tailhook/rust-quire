6bd7aad30e5d533b833c613625553b05b489c9c0
Geert Stappers <geert.stappers@hendrikx-itc.nl>
2019-08-04 20:08:57 +0200

    Added simple expample
    In the (new) example directory is now `simple.rs`
    and `config.yaml`. It is a working example plus a matching YAML file.

---
+++ examples/simple.rs
@@ -0,0 +1,31 @@
+extern crate quire;
+#[macro_use] extern crate serde_derive;
+use quire::{parse_config, Options};
+use quire::validate::{Structure, Scalar};
+
+#[derive(Deserialize)]
+#[allow(dead_code)]
+struct Config {
+    item1: String,
+    item2: Option<String>,
+}
+
+fn validator() -> Structure<'static> {
+    Structure::new()
+    .member("item1", Scalar::new())
+    .member("item2", Scalar::new().optional())
+}
+
+fn work(cfg: &Config) {
+    println!("item1 is {}.", cfg.item1);
+    //intln!("item2 is {}.", cfg.item2);
+    // hey, this is just demonstration code ...
+}
+
+fn main() {
+    let cfg: Config;
+    cfg = parse_config("examples/config.yaml",
+                       &validator(), &Options::default())
+        .expect("valid config");
+    work(&cfg)
+}
